815579ffc4085e7fc82627bf19b652d85a24d911
unknown <sameera@LT-0249.omobio.net>
2022-03-08 23:10:17 +0530

    change port

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("IT WORKED");
+  res.send("Port Changed to 8080");
 });
 app.get("/new", function (req, res) {
   res.send("New also worked");
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
 module.exports = app;
